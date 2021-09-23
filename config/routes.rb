fc0c65e05cee948a363ed5b71cff7722f5ddae21
EikiTsuchikiri <tsuchikiri.eiki.infratop@gmail.com>
2021-09-23 12:21:20 +0000

    [Finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root to: 'home#top'
+  get 'home/about'
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
